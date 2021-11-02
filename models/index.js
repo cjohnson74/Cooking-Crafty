af21f11237cf927f7c46423185fcd1d423fd27e7
Carson Johnson <carson74johnson@gmail.com>
2021-11-02 13:07:46 -0400

    added const variables for models

---
+++ models/index.js
@@ -0,0 +1,7 @@
+const User = require('./User');
+const Recipe = require('./Recipe');
+const Ingredient = require('./Ingredient');
+const UserRecipe = require('./UserRecipe');
+const UserIngredient = require('./UserIngredient');
+const RecipeIngredient = require('./RecipeIngredient');
+
