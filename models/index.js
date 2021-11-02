f0d920ee9fd71f96e91d02a6ad1f7c1268fcc13a
Carson Johnson <carson74johnson@gmail.com>
2021-11-02 13:26:05 -0400

    added user recipe relationship in models

--- models/index.js
+++ models/index.js
@@ -5,3 +5,7 @@ const UserRecipe = require('./UserRecipe');
 const UserIngredient = require('./UserIngredient');
 const RecipeIngredient = require('./RecipeIngredient');
 
+User.hasMany(Recipe, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
+});
