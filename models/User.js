c6dd84c19c0284122cd245423394a3b6177f9f1e
Carson Johnson <carson74johnson@gmail.com>
2021-11-02 14:19:22 -0400

    added user class that extends Model

--- models/User.js
+++ models/User.js
@@ -1,3 +1,9 @@
 const { Model, DataTypes } = require('sequelize');
 const bcrypt = require('bcrypt');
 const sequelize = require('../config/connection');
+
+class User extends Model {
+    checkPassword(loginPw) {
+        return bcrypt.compareSync(loginPw, this.password);
+    }
+}
