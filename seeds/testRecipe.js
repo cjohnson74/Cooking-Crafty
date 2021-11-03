94b574ed0fb093518e06a8eceb8b5a2b0f598bd6
Carson Johnson <carson74johnson@gmail.com>
2021-11-03 19:33:40 -0400

    creates recipe and ingredients arrays

--- seeds/testRecipe.js
+++ seeds/testRecipe.js
@@ -6,19 +6,41 @@ const searchedRecipe = 'egg and cheese';
   
 const properSearchRecipe = searchedRecipe.replace(/ /g,'%20');
 
+const recipeArray = [];
+const ingredientsArray = [];
+
 const url = `https://api.edamam.com/api/recipes/v2?type=public&q=${properSearchRecipe}&app_id=19224618&app_key=c855fa0b4fff67aaf62cd192e24a652b`;
         fetch(url)
             .then(function (response) {
                 return response.json();
             })
             .then(function (data) {
+
                 for(let i = 0; i < data.hits.length; i++ ) {
+                    // prints the recipe name to the console
                     console.log(data.hits[i].recipe.label);
+                    // prints the recipe url to the console
                     console.log(data.hits[i].recipe.url);
+                    var recipe = { name: data.hits[i].recipe.label, description: data.hits[i].recipe.url }
+                    // adds the recipe objects { name: "", url: "" } to the recipe array
+                    recipeArray[i] = JSON.stringify(recipe);
+
                     for (let j = 0; j < data.hits[i].recipe.ingredients.length; j++) {
-                    console.log(data.hits[i].recipe.ingredients[j].food);
+                        // console.log(data.hits[i].recipe.ingredients);
+                        // prints the ingredients to the console
+                        console.log(data.hits[i].recipe.ingredients[j].food);
+                        // adds the recipe objects { name: "", recipe_id: i } to the recipe array
+                        ingredientsArray.push(JSON.stringify({ name: data.hits[i].recipe.ingredients[j].food, recipe_id: i}));
                     }
                 }
+                // prints the recipeArray to the console
+                console.log(recipeArray);
+                // reads the recipeData.json file
+
+                //
+                // prints the ingredientsArray to the console
+                console.log(ingredientsArray);
+                // writes to the ingredie
             });
 }
 
