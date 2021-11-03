37f8cf701c312dd18efc61c4e3d9f53ba841a82a
Femi Ogun <femiogun21@yahoo.com>
2021-11-03 10:59:19 -0400

    Added auth.js. Added functionality on server.js to allow custom handlebars to work

--- server.js
+++ server.js
@@ -6,6 +6,11 @@ const sequelize = require('./config/connection');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+const hbs = exphbs.create({ helpers });
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
