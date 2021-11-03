09928719670e19eb126806d0b061a970dfede136
Carson Johnson <carson74johnson@gmail.com>
2021-11-02 21:19:50 -0400

    created the ingredient init for the Ingredient Model

--- models/Ingredient.js
+++ models/Ingredient.js
@@ -3,3 +3,20 @@ const sequelize = require('../config/connection');
 
 class Ingredient extends Model {}
 
+Ingredient.init(
+    {
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+    },
+    {
+        sequelize: sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'ingredient',
+    }
+);
+
+module.exports = Ingredient;
