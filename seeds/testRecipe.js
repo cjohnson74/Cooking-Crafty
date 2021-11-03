86e203f5af0f2ed81414ac6e5a5779f16458a247
Carson Johnson <carson74johnson@gmail.com>
2021-11-03 18:45:33 -0400

    added fetch to automate ingredients and recipe json fill

--- seeds/testRecipe.js
+++ seeds/testRecipe.js
@@ -1,42 +1,47 @@
 
-// const fetch = require('node-fetch');
+const fetch = require('node-fetch');
 
-// function fillIngredientJson() {
-// const searchedRecipe = 'egg and cheese';
+const fillIngredientJson = async => {
+const searchedRecipe = 'egg and cheese';
   
-// const properSearchRecipe = searchedRecipe.replace(/ /g,'%20');
+const properSearchRecipe = searchedRecipe.replace(/ /g,'%20');
 
-// const url = `https://api.edamam.com/api/recipes/v2?type=public&q=${properSearchRecipe}&app_id=19224618&app_key=c855fa0b4fff67aaf62cd192e24a652b`;
+const url = `https://api.edamam.com/api/recipes/v2?type=public&q=${properSearchRecipe}&app_id=19224618&app_key=c855fa0b4fff67aaf62cd192e24a652b`;
+        fetch(url)
+            .then(function (response) {
+                return response.json();
+            })
+            .then(function (data) {
+                for(let i = 0; i < data.hits.length; i++ ) {
+                    console.log(data.hits[i].recipe.label);
+                    console.log(data.hits[i].recipe.url);
+                    for (let j = 0; j < data.hits[i].recipe.ingredients.length; j++) {
+                    console.log(data.hits[i].recipe.ingredients[j].food);
+                    }
+                }
+            });
+}
 
-//     fetch(url)
-//         .then(function (response) {
-//         return response.json();
-//         })
-//         .then(function (data) {
-//         console.log(data)
-//         });
-// }
+fillIngredientJson();
 
-// fillIngredientJson();
-
-require('dotenv').config('../.env');
-const fetch = require('node-fetch')
-const fs = require('fs');
+// require('dotenv').config('../.env');
+// const fetch = require('node-fetch')
+// const fs = require('fs');
 
 
-const apiUrl = `https://api.edamam.com/api/recipes/v2?type=public`;
+// const apiUrl = `https://api.edamam.com/api/recipes/v2?type=public`;
 
-apiQuery = (query) => {
-    let apiCall = `${apiUrl}&q=peanut&app_id=19224618&app_key=c855fa0b4fff67aaf62cd192e24a652b`
+// apiQuery = (query) => {
+//     let apiCall = `${apiUrl}&q=peanut&app_id=19224618&app_key=c855fa0b4fff67aaf62cd192e24a652b`
 
-    fetch(apiCall)
-    .then(function (response) {
-        if(!response.ok) {
-            throw response.json();
-        }
+//     fetch(apiCall)
+//     .then(function (response) {
+//         if(!response.ok) {
+//             throw response.json();
+//         }
 
-        fs.writeFile('testing.json', response.json());
-    });
-};
+//         fs.writeFile('testing.json', response.json());
+//     });
+// };
 
-apiQuery();
+// apiQuery();
