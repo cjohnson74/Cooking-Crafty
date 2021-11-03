0c3126cf961bb2d51930efc1000c92b8eb43b525
Carson Johnson <carson74johnson@gmail.com>
2021-11-02 21:03:17 -0400

    added Recipe init to Recipe Model

--- models/Recipe.js
+++ models/Recipe.js
@@ -3,3 +3,32 @@ const sequelize = require('../config/connection');
 
 class Recipe extends Model {}
 
+Recipe.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        description: {
+            type: DataTypes.STRING,
+        },
+        date_created: {
+            type: DataTypes.DATE,
+            allowNull: false,
+            deafultValue: DataTypes.NOW,
+        },
+        user_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'user',
+                key: 'id',
+            },
+        },
+    },
+)
