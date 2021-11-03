7fe0bc6bee95740287ea9a2ae0df035eec0ecda9
Femi Ogun <femiogun21@yahoo.com>
2021-11-03 14:11:51 -0400

    Made changes to server.js

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ const express = require('express');
 const session = require('express-session');
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
+const exphbs = require('express-handlebars');
+const helpers = require('./utils/helpers');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express();
 const PORT = process.env.PORT || 3001;
