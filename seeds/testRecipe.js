ec95ac123cef461a890cedb4bd139899efc0a93e
Carson Johnson <carson74johnson@gmail.com>
2021-11-04 11:50:30 -0400

    added recipeIngredient array to hold the objects

--- seeds/testRecipe.js
+++ seeds/testRecipe.js
@@ -9,6 +9,8 @@ const properSearchRecipe = searchedRecipe.replace(/ /g,'%20');
 
 const recipeArray = [];
 const ingredientsArray = [];
+const recipeIngredientArray = [];
+
 
 const url = `https://api.edamam.com/api/recipes/v2?type=public&q=${properSearchRecipe}&app_id=19224618&app_key=c855fa0b4fff67aaf62cd192e24a652b`;
         fetch(url)
