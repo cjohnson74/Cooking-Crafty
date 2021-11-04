6adaeb5edc8b71aa6f2975be19d05072e008afa7
Carson Johnson <carson74johnson@gmail.com>
2021-11-04 10:27:59 -0400

    added recipeCount variable to keep track of the number of recipes in json

--- seeds/testRecipe.js
+++ seeds/testRecipe.js
@@ -1,11 +1,14 @@
 const fetch = require('node-fetch');
 const fs = require('fs');
 
+
+
 const fillIngredientJson = async => {
 const searchedRecipe = 'egg and cheese';
   
 const properSearchRecipe = searchedRecipe.replace(/ /g,'%20');
 
+var recipeCount;
 const recipeArray = [];
 const ingredientsArray = [];
 
@@ -16,21 +19,23 @@ const url = `https://api.edamam.com/api/recipes/v2?type=public&q=${properSearchR
             })
             .then(function (data) {
 
-                for(let i = 0; i < data.hits.length; i++ ) {
+                for(let i = 0; i < data.hits.length; i++) {
                     // prints the recipe name to the console
                     console.log(data.hits[i].recipe.label);
                     // prints the recipe url to the console
                     console.log(data.hits[i].recipe.url);
-                    var recipe = { name: data.hits[i].recipe.label, description: data.hits[i].recipe.url }
+                    var recipe = { name: data.hits[i].recipe.label, description: data.hits[i].recipe.url };
                     // adds the recipe objects { name: "", url: "" } to the recipe array
                     recipeArray[i] = recipe;
+                    // keeps track of the number of recipes in json
+                    recipeCount++;
 
                     for (let j = 0; j < data.hits[i].recipe.ingredients.length; j++) {
                         // console.log(data.hits[i].recipe.ingredients);
                         // prints the ingredients to the console
                         console.log(data.hits[i].recipe.ingredients[j].food);
                         // adds the recipe objects { name: "", recipe_id: i } to the recipe array
-                        var ingredientObject = { name: data.hits[i].recipe.ingredients[j].food, recipe_id: i };
+                        var ingredientObject = { name: data.hits[i].recipe.ingredients[j].food, recipe_id: recipeNumber };
                         ingredientsArray.push(ingredientObject);
                     }
                 }
