9dece35fdbbc189a8ba1572847a61451bc129bc7
Carson Johnson <carson74johnson@gmail.com>
2021-11-04 14:51:12 -0400

    fixed resetSeedsJSONFiles bug in helpers.js

--- utils/helpers.js
+++ utils/helpers.js
@@ -23,8 +23,7 @@ module.exports = {
   },
   // this resets a json file by writing an empty array to it
   resetSeedsJSONFiles: (filename) => {
-      var path = (`./seeds/${filename}.json`).toString;
-    fs.writeFileSync(path, [], (err) => {
+    fs.writeFileSync(`./seeds/${filename}.json`.toString(), [], (err) => {
         console.log(err);
     });
   },
